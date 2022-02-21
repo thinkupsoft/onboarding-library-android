d31619ce19d3b01cdcebae0ff4a6a67a829bb530
Santiago Rugnitz <santiagorugnitz@gmail.com>
2022-02-21 02:07:25 +0000

    lib

---
+++ lib/src/main/java/com/thinkup/onboardinglib/models/FadingSpotlight.kt
@@ -0,0 +1,39 @@
+package com.thinkup.onboardinglib.models
+
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+
+class FadingSpotlight(
+    rectangle: Rect,
+    isCircular: Boolean = false,
+    backgroundAlpha: Float = 0.8f,
+    animationSpec: AnimationSpec<Float> = tween(durationMillis = 500, easing = LinearEasing),
+    hint: @Composable () -> Unit
+) : BaseSpotlight(
+    rectangle = rectangle,
+    isCircular = isCircular,
+    backgroundAlpha = backgroundAlpha,
+    animationSpec = animationSpec,
+    hint = hint
+) {
+
+    override val initialAnimatedValue: Float
+        get() = 1f
+
+    override val finalAnimatedValue: Float
+        get() = 0f
+
+    override fun getPathRect(size: Size, animatedValue: Float): Rect {
+        return if (isCircular) {
+            Rect(center = rectangle.center, radius = rectangle.height / 2)
+        } else {
+            rectangle
+        }
+    }
+
+    override fun getAlpha(animatedValue: Float) = backgroundAlpha * (1-animatedValue)
+}
